9e5799977fe4a68f68b61dbf8abe11809436e764
Kosto <152010149+Kosto179cn@users.noreply.github.com>
2025-09-03 13:34:37 +0800

    Update and rename test.js to test.script.user.js

---
+++ newjs/test.script.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Kosto云端用户版本
+// @namespace    http://tampermonkey.net/
+// @version      10
+// @author       CN@Kosto1797867628
+// @match        https://*.3dtank.com/play/*
+// @match        https://*.tankionline.com/play/*
+// @include      https://*.test-*.tankionline.com/*
+// @icon         https://gitee.com/Kosto179/kosto-self-use-script/raw/master/KostoIcon.jpg
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest 
+// @connect      gitee.com
+// @connect      github.io
+// ==/UserScript==
