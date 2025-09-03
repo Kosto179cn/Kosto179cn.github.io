e0156c2b749288685ff13120ef3da68985767878
Kosto <152010149+Kosto179cn@users.noreply.github.com>
2025-09-03 13:34:52 +0800

    Update test.script.user.js

--- newjs/test.script.user.js
+++ newjs/test.script.user.js
@@ -1,14 +1,11 @@
 // ==UserScript==
 // @name         Kosto云端用户版本
 // @namespace    http://tampermonkey.net/
-// @version      10
+// @version      25.9.9999
 // @author       CN@Kosto1797867628
 // @match        https://*.3dtank.com/play/*
 // @match        https://*.tankionline.com/play/*
 // @include      https://*.test-*.tankionline.com/*
 // @icon         https://gitee.com/Kosto179/kosto-self-use-script/raw/master/KostoIcon.jpg
 // @run-at       document-start
-// @grant        GM_xmlhttpRequest 
-// @connect      gitee.com
-// @connect      github.io
 // ==/UserScript==
