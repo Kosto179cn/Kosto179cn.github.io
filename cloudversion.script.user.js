bb8a4fac541040d400ad9f845f56f49089c8a6b9
Kosto <152010149+Kosto179cn@users.noreply.github.com>
2025-09-24 17:14:20 +0800

    Update cloudversion.script.user.js via editor

--- cloudversion.script.user.js
+++ cloudversion.script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kosto云端用户版本
 // @namespace    http://tampermonkey.net/
-// @version      25
+// @version      24
 // @author       CN@Kosto1797867628
 // @match        https://*.3dtank.com/play/*
 // @match        https://*.tankionline.com/play/*
