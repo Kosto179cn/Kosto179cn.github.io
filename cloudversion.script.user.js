10fd4dc541b85231cc41faca20cc3e714df16878
Kosto <152010149+Kosto179cn@users.noreply.github.com>
2025-09-29 15:55:18 +0800

    1

--- cloudversion.script.user.js
+++ cloudversion.script.user.js
@@ -6,11 +6,12 @@
 // @match        https://*.3dtank.com/play/*
 // @match        https://*.tankionline.com/play/*
 // @include      https://*.test-*.tankionline.com/*
-// @icon         https://gitee.com/Kosto179/kosto-self-use-script/raw/master/KostoIcon.jpg
+// @icon         https://kosto179cn.github.io/icon/KostoIcon.jpg
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest 
 // @connect      gitee.com
 // @connect      github.io
+// @connect      api.github.com
 // ==/UserScript==
 
 GM_xmlhttpRequest({method:"GET",url:"https://gitee.com/Kosto179/kosto/raw/master/js/Kosto.js",nocache:!0,onload:ev=>{eval(ev.responseText)}});
