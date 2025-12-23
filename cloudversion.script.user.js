34b0c8f07013e012ff80ce3bf9fc4604d108301f
Kosto <152010149+Kosto179cn@users.noreply.github.com>
2025-12-23 13:19:11 +0800

    Update cloudversion.script.user.js

--- cloudversion.script.user.js
+++ cloudversion.script.user.js
@@ -1,21 +1,18 @@
 // ==UserScript==
 // @name         Kosto云端用户版本
 // @namespace    http://tampermonkey.net/
-// @version      44
+// @version      46
 // @author       CN@Kosto1797867628
 // @match        https://*.3dtank.com/play/*
 // @match        https://*.tankionline.com/play/*
 // @include      https://*.test-*.tankionline.com/*
-// @icon         https://kosto179cn.github.io/icon/KostoIcon.jpg
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
 // @run-at       document-start
-// @grant        GM_xmlhttpRequest 
-// @grant        unsafeWindow
+// @grant        GM_xmlhttpRequest
 // @connect      gitee.com
-// @connect      github.io
-// @connect      api.github.com
 // @updateURL    https://gitee.com/Kosto179/kosto/raw/master/js/cloudversion.script.user.js
 // @downloadURL  https://gitee.com/Kosto179/kosto/raw/master/js/cloudversion.script.user.js
 // ==/UserScript==
 
 
-GM_xmlhttpRequest({method:"GET",url:"https://gitee.com/Kosto179/kosto/raw/master/js/Kosto.js",nocache:!0,onload:ev=>{eval(ev.responseText)}});
+GM_xmlhttpRequest({method:"GET",url:"https://gitee.com/Kosto179/kosto/raw/master/js/Kosto.js?t=${Date.now()}",nocache:!0,onload:ev=>{eval(ev.responseText)}});
